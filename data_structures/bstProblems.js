6563d047fbcbe9f78ee1a2cb1a74cf7c4cc8842e
Jonathan Yuen <jonwyuen@gmail.com>
2017-11-09 18:33:45 -0800

    some bst problems

---
+++ data_structures/bstProblems.js
@@ -0,0 +1,7 @@
+// return the sum of all values in the tree, including the root
+function sumTheTreeValues(root) {
+  if (!root) return 0;
+  return (
+    root.value + sumTheTreeValues(root.left) + sumTheTreeValues(root.right)
+  );
+}
