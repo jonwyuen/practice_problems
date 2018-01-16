ee71fd5ed018c1fa7c471255f1ef2099b6b1995b
Jonathan Yuen <jonwyuen@gmail.com>
2018-01-15 21:32:56 -0800

    reverse array in place

---
+++ js_algs/reverseArrayInPlace.js
@@ -0,0 +1,17 @@
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+
+  return arr;
+}
+
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+  }
+
+  return arr;
+}
