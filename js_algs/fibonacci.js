263cf1f40f73c08413a092467bdff4577becd046
Jonathan Yuen <jonwyuen@gmail.com>
2018-03-05 02:59:18 -0800

    fib memo

--- js_algs/fibonacci.js
+++ js_algs/fibonacci.js
@@ -3,3 +3,10 @@ function fibonacci(n) {
   if (n < 3) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+// Time Complexity: O(n)
+function fibMemo(n, cache = {}) {
+  if (cache[n]) return cache[n];
+  if (n < 3) return 1;
+  return (cache[n] = fibMemo(n - 1, cache) + fibMemo(n - 2, cache));
+}
