20ebb7a4472d12a4ff016e2904e75d53b485e073
Jonathan Yuen <jonwyuen@gmail.com>
2018-03-09 00:50:09 -0800

    mergeSort

--- js_algs/mergeSort.js
+++ js_algs/mergeSort.js
@@ -3,9 +3,14 @@ function merge(arr1, arr2) {
 
   while (arr1.length && arr2.length) {
     let minElem;
-
     if (arr1[0] < arr2[0]) minElem = arr1.shift();
+    else minElem = arr2.shift();
+    result.push(minElem);
   }
+
+  if (arr1.length) result = result.concat(arr1);
+  else result = result.concat(arr2);
+  return result;
 }
 
 function mergeSort(arr) {
@@ -13,4 +18,6 @@ function mergeSort(arr) {
   let mid = Math.floor(arr.length / 2);
   let firstHalf = arr.slice(0, mid);
   let secondHalf = arr.slice(mid);
+
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf));
 }
