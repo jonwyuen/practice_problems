95bcab0620a8d646973acbb47088d19afe8649f3
Jonathan Yuen <jonwyuen@gmail.com>
2018-03-21 00:33:42 -0700

    steps recursive

--- js_algs/more_algs/steps.js
+++ js_algs/more_algs/steps.js
@@ -9,4 +9,15 @@ function steps(n) {
   }
 }
 
-function steps(n) {}
+function steps(n, row = 0, stair = "") {
+  if (n === row) return;
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, ++row);
+  }
+
+  if (stair.length <= row) stair += "#";
+  else stair += " ";
+
+  steps(n, row, stair);
+}
