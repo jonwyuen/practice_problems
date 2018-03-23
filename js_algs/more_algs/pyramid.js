c8fce0ac098d96e260a4c040abc2431b5c83395a
Jonathan Yuen <jonwyuen@gmail.com>
2018-03-23 03:02:33 -0700

    pyramid iterative

--- js_algs/more_algs/pyramid.js
+++ js_algs/more_algs/pyramid.js
@@ -1,4 +1,15 @@
-function pyramid(n) {}
+function pyramid(n) {
+  const mid = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (mid - row <= col && mid + row >= col) level += "#";
+      else level += " ";
+    }
+    console.log(level);
+  }
+}
 
 pyramid(3);
 
