ccc7e791373472074adc037a9026150e3c5df6d3
Jonathan Yuen <jonwyuen@gmail.com>
2018-03-30 01:29:05 -0700

    fib memo

--- js_algs/more_algs/fib.js
+++ js_algs/more_algs/fib.js
@@ -1,3 +1,5 @@
+// Iterative O(n) linear
+
 function fib(n) {
   const result = [0, 1];
 
@@ -10,8 +12,37 @@ function fib(n) {
   return result[n];
 }
 
+// Recursive O(2^n) exponential
+
 function fib(n) {
   if (n < 2) return n;
 
   return fib(n - 1) + fib(n - 2);
 }
+
+// Memoization O()
+
+function fib(n, cache = {}) {
+  if (cache[n]) return cache[n];
+  if (n < 2) return n;
+  return (cache[n] = fib(n - 1) + fib(n - 2));
+}
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) return cache[args];
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) return n;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
