28b5bb4ec2c8a14d90f900b5281aaed3ee6dfe63
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-01 00:02:59 -0700

    queue

---
+++ js_algs/more_algs/queue.js
@@ -0,0 +1,22 @@
+/*
+First in first out
+*/
+
+class Queue() {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length-1];
+  }
+
+}
