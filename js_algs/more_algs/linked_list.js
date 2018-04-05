d5768b0a848e153bc7574f7be598f2c5939f8c55
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-05 03:34:45 -0700

    some linked list methods

--- js_algs/more_algs/linked_list.js
+++ js_algs/more_algs/linked_list.js
@@ -1,4 +1,51 @@
-class LinkedList() {
-
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let size = 0;
+    let node = this.head;
+    while (node) {
+      size++;
+      node = node.next;
+    }
+
+    return size;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) return null;
+    let node = this.head;
+
+    while (node.next) {
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+}
