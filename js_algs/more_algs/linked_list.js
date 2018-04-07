c71d08b3f1fb47ad29bc8bd4be0ce0d7286a2670
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-07 01:00:27 -0700

    more linked list

--- js_algs/more_algs/linked_list.js
+++ js_algs/more_algs/linked_list.js
@@ -48,4 +48,70 @@ class LinkedList {
     if (!this.head) return;
     this.head = this.head.next;
   }
+
+  removeLast() {
+    if (!this.head) return;
+    if (!this.head.next) return this.head;
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    let last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let node = this.head;
+    let counter = 0;
+
+    while (node) {
+      if (index === counter) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) return;
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let previous = this.getAt(index - 1);
+    if (!previous || !previous.next) return;
+    previous.next = previous.next.next;
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    let previous = this.getAt(index - 1) || this.getLast();
+    let node = new Node(data, previous.next);
+    previous.next = node;
+  }
 }
