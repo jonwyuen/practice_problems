5a13efccabfe810a46b2fb757b8805670de3afc8
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-14 23:17:58 -0700

    fromlast

--- js_algs/more_algs/linked_list.js
+++ js_algs/more_algs/linked_list.js
@@ -166,3 +166,20 @@ function circular(list) {
 
   return false;
 }
+
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast  fast.next;
+  }
+
+  return slow;
+}
