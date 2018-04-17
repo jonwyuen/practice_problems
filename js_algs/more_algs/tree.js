55b0657520522ba265d6a5b63649e30af0f7f645
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-16 23:57:51 -0700

    tree

--- js_algs/more_algs/tree.js
+++ js_algs/more_algs/tree.js
@@ -1,1 +1,14 @@
-class Tree {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+}
+
+class Tree {
+  constructor() {}
+}
