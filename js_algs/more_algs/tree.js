d7f643ab3398131ff0ea572b8befd02ce62b4ba0
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-18 23:39:40 -0700

    tree bfs

--- js_algs/more_algs/tree.js
+++ js_algs/more_algs/tree.js
@@ -14,5 +14,16 @@ class Node {
 }
 
 class Tree {
-  constructor() {}
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
 }
