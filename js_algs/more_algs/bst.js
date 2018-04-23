389f3a701602c5ddd0d9ec8c891378d32e338ab3
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-23 01:28:38 -0700

    validate bst

--- js_algs/more_algs/bst.js
+++ js_algs/more_algs/bst.js
@@ -32,3 +32,12 @@ class Node {
   }
 }
 
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data > max) return false;
+  if (min !== null && node.data < max) return false;
+
+  if (node.left && !validate(node.left, min, node.data)) return false;
+  if (node.right && !validate(node.right, node.data, max)) return false;
+
+  return true;
+}
