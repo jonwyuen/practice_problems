38d6170782b69de97607e6b96fd4848422a356b7
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-26 23:05:29 -0700

    bubble sort

---
+++ js_algs/more_algs/sorting.js
@@ -0,0 +1,19 @@
+/*
+bubbleSort: n^2
+selectionSort: n^2
+mergeSort: nlogn
+*/
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+
+  return arr;
+}
