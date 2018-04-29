9122e74ad8c8703914497f6235010a036b43aa48
Jonathan Yuen <jonwyuen@gmail.com>
2018-04-29 00:37:06 -0700

    mergesort

--- js_algs/more_algs/sorting.js
+++ js_algs/more_algs/sorting.js
@@ -36,10 +36,18 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
 
 function merge(left, right) {
-  let results = [];
+  const results = [];
 
   while (left.length && right.length) {
     if (left[0] < right[0]) results.push(left.shift());
